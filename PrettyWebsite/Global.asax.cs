40a4cecc59294a25c25949117ba803907a9edf90
Wikenbrant <matte955@live.se>
2020-09-10 11:04:37 +0200

    Add project files.

---
+++ PrettyWebsite/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace PrettyWebsite
+{
+    public class EPiServerApplication : EPiServer.Global
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+
+            //Tip: Want to call the EPiServer API on startup? Add an initialization module instead (Add -> New Item.. -> EPiServer -> Initialization Module)
+        }
+    }
+}
