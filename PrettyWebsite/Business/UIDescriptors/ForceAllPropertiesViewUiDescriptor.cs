e3c806ad16d69ea66aacc43beb08a6eac406b812
47720480 <david.n.stahl@gmail.com>
2020-09-11 10:50:29 +0200

    UIDescriptors added

---
+++ PrettyWebsite/Business/UIDescriptors/ForceAllPropertiesViewUiDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Shell;
+using PrettyWebsite.Models.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrettyWebsite.Business.UIDescriptors
+{
+    [UIDescriptorRegistration]
+    public class ForceAllPropertiesViewUiDescriptor : UIDescriptor<StartPage>
+    {
+        public ForceAllPropertiesViewUiDescriptor()
+        {
+            DefaultView = CmsViewNames.AllPropertiesView;
+        }
+    }
+}
---
+++ PrettyWebsite/Business/UIDescriptors/TreeIcons/ArticleTreeIconDescriptor.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Shell;
+using PrettyWebsite.EpiServerDefaultIcon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrettyWebsite.Business.UIDescriptors.TreeIcons
+{
+    [UIDescriptorRegistration]
+    public class ArticleTreeIconDescriptor : UIDescriptor<IUseArticleTreeIcon>
+    {
+        public ArticleTreeIconDescriptor()
+        {
+            IconClass = EpiserverDefaultContentIcons.ObjectIcons.Category;
+        }
+    }
+    public interface IUseArticleTreeIcon
+    {
+    }
+}
