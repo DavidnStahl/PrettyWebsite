2ec2cc55dafac325b5d22b9d5f7d965a4fa64834
Wikenbrant <55382118+Wikenbrant@users.noreply.github.com>
2020-09-14 10:47:27 +0200

    fixat mer bas klasser

---
+++ PrettyWebsite/Controllers/Base/BasePartialController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Core;
+using EPiServer.Web.Mvc;
+
+namespace PrettyWebsite.Controllers.Base
+{
+    public class BasePartialController<T> : PartialContentController<T> where T : IContentData
+    {
+
+    }
+}
---
+++ PrettyWebsite/Models/Blocks/SiteBlockData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Core;
+
+namespace PrettyWebsite.Models.Blocks
+{
+    public class SiteBlockData : BlockData
+    {
+    }
+}
