daeed5525c6942db5dc04fe52207f6e66c153b0e
47720480 <david.n.stahl@gmail.com>
2020-09-14 14:39:08 +0200

    save

---
+++ PrettyWebsite/Controllers/Pages/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using PrettyWebsite.Models.Pages;
+using PrettyWebsite.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrettyWebsite.Controllers.Pages
+{
+    
+    public class SearchController : PageControllerBase<StartPage>
+    {
+        public ActionResult Index(StartPage currentPage, string query)
+        {
+            
+
+            var model = new StartPageViewModel(currentPage);
+
+            return View(model);
+        }
+    }
+}
