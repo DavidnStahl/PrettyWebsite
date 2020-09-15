ae6a6737fdba350b5608d84f1e3a9f31eb5a5878
Marcus Sandin <55379421+MarcusSandin@users.noreply.github.com>
2020-09-15 09:01:18 +0200

    MovieViewModel

---
+++ PrettyWebsite/Models/ViewModels/MovieViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PrettyWebsite.Models.Pages;
+using PrettyWebsite.Models.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrettyWebsite.Models.ViewModels
+{
+    public class MovieViewModel : PageViewModel<SearchPage>
+    {
+        public Movie Movie { get; }
+            
+        public MovieViewModel(SearchPage currentPage, Movie movie) : base(currentPage)
+        {
+            Movie = movie;
+        }
+    }
+}
