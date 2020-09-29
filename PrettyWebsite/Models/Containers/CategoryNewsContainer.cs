4e4f5ae8cd53797420d1555ae8c3c4c1dc3f8b25
Wikenbrant <55382118+Wikenbrant@users.noreply.github.com>
2020-09-29 13:36:56 +0200

    fixat category container

---
+++ PrettyWebsite/Models/Containers/CategoryNewsContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using PrettyWebsite.Models.Pages;
+
+namespace PrettyWebsite.Models.Containers
+{
+    [ContentType(DisplayName = "Category Container", GUID = "70bd6db9-fda0-46dc-a6f5-741f43f8cd60", Description = "")]
+    [AvailableContentTypes(
+        Availability.Specific,
+        Include = new[]
+        {
+            typeof(NewsPage),
+        }
+    )]
+    public class CategoryNewsContainer : NewsContainer
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
