b1c577c31cf6c3e1f682988bca213b85c29cfbf3
Wikenbrant <55382118+Wikenbrant@users.noreply.github.com>
2020-09-29 14:16:54 +0200

    Fixat ny menysystem

--- PrettyWebsite/Controllers/Blocks/HeaderBlockController.cs
+++ PrettyWebsite/Controllers/Blocks/HeaderBlockController.cs
@@ -8,6 +8,7 @@ using EPiServer.Core;
 using EPiServer.Web;
 using EPiServer.Web.Mvc;
 using PrettyWebsite.Models.Blocks;
+using PrettyWebsite.Models.Containers;
 using PrettyWebsite.Models.Pages;
 using PrettyWebsite.Models.ViewModels;
 using PrettyWebsite.Models.ViewModels.Blocks;
@@ -26,14 +27,34 @@ namespace PrettyWebsite.Controllers.Blocks
         {
             var startPageContentLink = SiteDefinition.Current.StartPage;
 
+            var menu = new Dictionary<CategoryNewsContainer, IEnumerable<SitePageData>>();
+
             var startPage = _contentLoader.Get<StartPage>(startPageContentLink);
-            var menu = _contentLoader.GetChildren<SitePageData>(startPage.ContentLink).Where(x => x.VisibleInMenu).ToList();
-            menu.Insert(0, startPage);
+            var newsContainer = _contentLoader.GetChildren<NewsContainer>(startPage.ContentLink).FirstOrDefault(x => x.VisibleInMenu);
+
+            if (newsContainer is NewsContainer container)
+            {
+
+                var categoryNewsContainers = _contentLoader
+                    .GetChildren<CategoryNewsContainer>(container.ContentLink)
+                    .Where(x => x.VisibleInMenu);
+
+                foreach (var categoryContainer in categoryNewsContainers)
+                {
+                    menu.Add(
+                        categoryContainer, 
+                        _contentLoader.GetChildren<SitePageData>(categoryContainer.ContentLink).Where(x => x.VisibleInMenu)
+                        );
+                }
+            }
+
+            //var menu = _contentLoader.GetChildren<CategoryNewsContainer>(newsContainer.ContentLink).Where(x => x.VisibleInMenu).ToList();
 
             var model = new HeaderBlockViewModel(currentBlock)
             {
                 Navigation = new NavigationViewModel
                 {
+                    StartPage = startPage,
                     Menu = menu                   
                 }                  
             };
