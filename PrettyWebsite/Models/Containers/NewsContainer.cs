70501b78414fbb7c619928e7bc9ad1a67c0929ee
47720480 <david.n.stahl@gmail.com>
2020-09-30 09:28:01 +0200

    update

--- PrettyWebsite/Models/Containers/NewsContainer.cs
+++ PrettyWebsite/Models/Containers/NewsContainer.cs
@@ -24,6 +24,7 @@ namespace PrettyWebsite.Models.Containers
     )]
     public class NewsContainer : SitePageData, IUseFolderIcon, IExcludeFromSitemap
     {
+
         /*
                 [CultureSpecific]
                 [Display(
