1cfe1231d532b582ca8ad0cffb69b800850bfb0b
David Stahl <david.n.stahl@gmail.com>
2020-11-23 08:56:02 +0100

    last change

---
+++ PrettyWebsite/Business/Personalization/LoggedIn/LoggedInCriterion.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Personalization.VisitorGroups;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace PrettyWebsite.Business.Personalization.LoggedIn
+{
+    [VisitorGroupCriterion(
+         Category = "Technical",
+         DisplayName = "Logged in",
+         Description = "Checks if a logged in cookie exists"
+     )]
+    public class LoggedInCriterion : CriterionBase<LoggedInCriterionSettings>
+    {
+        public override bool IsMatch(IPrincipal principal, HttpContextBase httpContext)
+        {
+            return HttpContext.Current.User.Identity.IsAuthenticated;
+        }
+    }
+}
---
+++ PrettyWebsite/Business/Personalization/LoggedIn/LoggedInCriterionSettings.cs
@@ -0,0 +1,16 @@
+﻿using EPiServer.Personalization.VisitorGroups;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrettyWebsite.Business.Personalization.LoggedIn
+{
+    public class LoggedInCriterionSettings : CriterionModelBase
+    {
+        public override ICriterionModel Copy()
+        {
+            return ShallowCopy();
+        }
+    }
+}
